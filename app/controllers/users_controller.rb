f3502f2765a03926946b2748cb4c4545e1ba6b2d
Emilio Corpus <emiliocorpus@gmail.com>
2015-08-23 02:12:03 -0700

    adds all .html files with blue banner and logout button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
   include UsersHelper
+  include SessionsHelper
 
   def create
       new_user_params = params[:new_user]
 
       if new_user_params[:password] == new_user_params[:confirm_password]
-        @user =  User.new(first_name: new_user_params[:first_name],
+        user =  User.new(first_name: new_user_params[:first_name],
                           last_name: new_user_params[:last_name],
                           username: new_user_params[:username],
                           email: new_user_params[:email],
                           password: new_user_params[:password])
-        if @user.save
-          @user
-          redirect_to profile_path(@user)
+        if user.save
+          log_in user
+          redirect_to profile_path(user)
         else
           redirect_to root_path
         end
@@ -36,5 +37,6 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,17 @@ Rails.application.routes.draw do
 	post 'login' => 'sessions#create'
 	delete 'sessions/:id' => 'sessions#destroy', as: 'logout' 
 
-	get 'edit_profile' => 'users#edit'
-	get 's/:id' => 'users#show', as: 'profile'
+	get 's/:id/edit' => 'users#edit', as: "edit_profile"
+	
 	post 'users/new' => 'users#create', as: 'sign_up'
 	get 'hunts/' => 'hunts#index', as: 'hunts'
 	
 
-	resources :s, :except => [:index, :new, :edit, :show, :create], controller: "users", shallow: true 
+	resources :s, :except => [:index, :new, :edit, :create], controller: "users", shallow: true 
 
 	resources :hunts, controller: "hunts", :except => [:index, :new] do
 			    resources :clues
 	end
 
-
+	get 's/:id' => 'users#show', as: 'profile'
 end
